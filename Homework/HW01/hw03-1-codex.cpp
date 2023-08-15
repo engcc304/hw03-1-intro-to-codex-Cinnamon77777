8f7cf04ed0c01d89cf2c16239ccd4325378c3897
your name <sriwljitthitima@gmail.com>
2023-08-16 00:13:52 +0700

    up

--- Homework/HW01/hw03-1-codex.cpp
+++ Homework/HW01/hw03-1-codex.cpp
@@ -2,8 +2,8 @@
 #include <stdio.h>
 int main()
 {
-  int a = 10;
-  int b = 20, c = 30, d = 40;
+  int a = 10 ;
+  int b = 20, c = 30, d = 40 ;
   printf( "This is %d + %d = %d ", a, b, a+b);
   for (int i = 0 ; i < d - 30 ; i++ )
   {
